a4116ecbc049a93f365b1614e66bec0dbb3f2abb
c-Tos1wa <cristoshiwassaki@gmail.com>
2022-08-18 16:10:37 -0300

    redireciona pra próxima página

--- front/src/pages/home/index.jsx
+++ front/src/pages/home/index.jsx
@@ -1,4 +1,5 @@
-import {  useRef } from 'react'
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import figure from "../../assets/image.svg";
 
@@ -8,48 +9,52 @@ import * as styles from '../../../style/general'
 
 export function Upload() {
   const fileUploaded = useRef(null)
+  const sendTo = useNavigate()
 
   const handleChange = (e) => {
     console.log(e.target.files[0].name)
+    sendTo('/loading')
   }
 
   const handleClick = () => {
     fileUploaded.current.click()
   }
 
+  
 
-  return(
+
+  return (
     <styles.ALL>
       <UPLoad>
         <styles.H2>Upload your image</styles.H2>
 
         <styles.P>File should be Jpeg, Png....</styles.P>
 
-        <Card
-      onDragOver={() => handleDragOver()}
-      onDrop={() => handleDrop()}
-    >
-      <img
-        src={figure}
-        alt="figura de duas montanhas em meio a nuvens na frente de um circulo azul"
-      />
+        <Card>
+          <img
+            src={figure}
+            alt="figura de duas montanhas em meio a nuvens na frente de um circulo azul"
+          />
 
-      <styles.Span>Drag &amp; Drop your images here</styles.Span>
-    </Card>
+          <styles.Span>Drag &amp; Drop your images here</styles.Span>
+        </Card>
 
         <styles.P>Or</styles.P>
 
-        <input 
+        <input
           type="file"
-          accept="image/*" 
-          style={{display: "none"}}
+          accept="image/*"
+          style={{
+            display: "none"
+          }}
           ref={fileUploaded}
           onChange={handleChange}
-          />
+        />
+
         <Choose
           onClick={() => handleClick()}
         >Choose a file</Choose>
-      
+
       </UPLoad>
     </styles.ALL>
   )
